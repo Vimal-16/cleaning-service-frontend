d52317bd0f79344e6d56b32619ad9fceeee099b6
Vimal-16 <vimalvelusamy16@gmail.com>
2026-02-13 20:30:15 +0530

    Update API URLs for production

--- src/BookingForm.jsx
+++ src/BookingForm.jsx
@@ -25,7 +25,7 @@ function BookingForm() {
     e.preventDefault();
     
     try {
-      const response = await fetch('http://localhost:5000/api/bookings/create', {
+      const response = await fetch('https://cleaning-service-backend-avjj.onrender.com//api/bookings/create', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -19,7 +19,7 @@ function Signup() {
     e.preventDefault();
     
     try {
-      const response = await fetch('http://localhost:5000/api/auth/signup', {
+      const response = await fetch('https://cleaning-service-backend-avjj.onrender.com//api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
